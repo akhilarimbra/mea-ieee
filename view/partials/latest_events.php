0b35bf6e3bf3484b7956a63ae855a7d434e65d58
Akhil Raj <nilamburakhil@gmail.com>
2016-05-14 19:39:02 +0530

    final commit

--- view/partials/latest_events.php
+++ view/partials/latest_events.php
@@ -5,7 +5,6 @@
       <h2 style="color: black;padding-bottom: .5em;">Latest Events : </h2>
     </div>
   </div>
-  <div class="row">
         <?php  
           $sql = "SELECT * FROM `event` ORDER BY date DESC LIMIT 0,4";
           $result = $conn->query($sql);
@@ -30,24 +29,4 @@
               echo "</div><br>";
           }
         ?>
-
-  </div>
-  <div class="row">
-    <div class="col-md-3">
-      <img class='shadow-effect' src="http://www.shoutmeloud.com/wp-content/uploads/2016/05/Factor-for-Success-of-Startup-274x164.png" width='100%' />
-      <h3 style="color: black;text-align: center;">How to Say No To Your Clients Without Feeling Guilty</h3>
-    </div>
-    <div class="col-md-3">
-      <img class="shadow-effect" src="http://www.shoutmeloud.com/wp-content/uploads/2016/05/Checklist-for-Profitable-Startup-274x164.png" width='100%' />
-      <h3 style="color: black;text-align: center;">How to Say No To Your Clients Without Feeling Guilty</h3>
-    </div>
-    <div class="col-md-3">
-      <img class="shadow-effect" src="http://www.shoutmeloud.com/wp-content/uploads/2016/05/Tax-Registration-for-Services-Outside-India-274x164.png" width='100%' />
-      <h3 style="color: black;text-align: center;">How to Say No To Your Clients Without Feeling Guilty</h3>
-    </div>
-    <div class="col-md-3">
-      <img class="shadow-effect" src="http://www.shoutmeloud.com/wp-content/uploads/2016/05/Factor-for-Success-of-Startup-274x164.png" width='100%' />
-      <h3 style="color: black;text-align: center;">How to Say No To Your Clients Without Feeling Guilty</h3>
-    </div>
-  </div>
 </div>
